03d6da4ead5d26adb24c8b2cdca338f040f016de
richie-south <richard.soderman@gmail.com>
2018-05-16 13:47:09 +0200

    refactor: remove unnessesary switch

--- app/date-helper.ts
+++ app/date-helper.ts
@@ -18,54 +18,26 @@ export const getDateOfType = (type: PartsType, matchTime: Date) => {
 }
 
 export const getActiveBasedOnDate = (
-  data: Array<Array<number>>,
+  cronParts: Array<Array<number>>,
   matchTime = new Date()
 ) => {
-  const checkWithMatchTime = (type: PartsType, time: number) => {
-    switch (type) {
-      case PartNames.minutes:
-        return matchTime.getMinutes() === time ? time : null
-      case PartNames.hours:
-        return matchTime.getHours() === time ? time : null
-      case PartNames.dayOfMonth:
-        return matchTime.getDate() === time ? time : null
-      case PartNames.monthOfYear:
-        return matchTime.getMonth() === time ? time : null
-      case PartNames.dayOfWeek: {
-        return matchTime.getDay() === time ? time : null
+  const activeCronParts = cronParts.map((times, i) =>
+    times.map(time => {
+      switch (i) {
+        case 0:
+          return matchTime.getMinutes() === time ? time : null
+        case 1:
+          return matchTime.getHours() === time ? time : null
+        case 2:
+          return matchTime.getDate() === time ? time : null
+        case 3:
+          return matchTime.getMonth() === time ? time : null
+        case 4:
+          return matchTime.getDay() === time ? time : null
+        case 5:
+          return matchTime.getFullYear() === time ? time : null
       }
-      case PartNames.year:
-        return matchTime.getFullYear() === time ? time : null
-    }
-  }
-
-  const [minutes, hours, daysInMonth, monthOfYear, dayOfWeek, year] = data.map(
-    (times, i) =>
-      times.map(time => {
-        switch (i) {
-          case 0:
-            return checkWithMatchTime(PartNames.minutes, time)
-          case 1:
-            return checkWithMatchTime(PartNames.hours, time)
-          case 2:
-            return checkWithMatchTime(PartNames.dayOfMonth, time)
-          case 3:
-            return checkWithMatchTime(PartNames.monthOfYear, time)
-          case 4:
-            return checkWithMatchTime(PartNames.dayOfWeek, time)
-          case 5:
-            return checkWithMatchTime(PartNames.year, time)
-        }
-      })
+    })
   )
-  const removeNull = value => value !== null
-
-  return [
-    minutes.filter(removeNull),
-    hours.filter(removeNull),
-    daysInMonth.filter(removeNull),
-    monthOfYear.filter(removeNull),
-    dayOfWeek.filter(removeNull),
-    year.filter(removeNull)
-  ]
+  return activeCronParts.map(value => value.filter(value => value !== null))
 }
